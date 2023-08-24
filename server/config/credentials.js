bad5ce9359ae8ac1d0f883dd5ae8e68472aca612
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-24 15:40:39 +0800

    完善文档

--- server/config/credentials.js
+++ server/config/credentials.js
@@ -1,3 +1,10 @@
+/**
+ * Module exports an object containing credentials for qqmail service.
+ * @module credentials
+ * @type {Object}
+ * @property {string} user - The email address of the qqmail account.
+ * @property {string} pass - The password of the qqmail account.
+ */
 module.exports = {
     qqmail:{
         user: '2320608933@qq.com',
--- server/dao/emailserver.js
+++ server/dao/emailserver.js
@@ -1,7 +1,16 @@
+/**
+ * Module dependencies.
+ * @module emailserver
+ */
+
 var nodemailer = require('nodemailer');
 
 var credentials = require('../config/credentials');
 
+/**
+ * Creates a nodemailer transporter object with the specified service and authentication credentials.
+ * @type {Object}
+ */
 var transporter = nodemailer.createTransport({
     service: 'qq',
     auth:{
@@ -10,6 +19,11 @@ var transporter = nodemailer.createTransport({
     }
 })
 
+/**
+ * Sends a verification email to the specified email address.
+ * @param {string} email - The email address to send the verification email to.
+ * @param {Object} res - The response object to send the result of the email sending operation to.
+ */
 exports.emailSignUp = function(email, res){
     let options ={
         from: credentials.qqmail.user,
@@ -17,12 +31,14 @@ exports.emailSignUp = function(email, res){
         subject: 'Cactus注册验证码',
         html: '<h1>欢迎注册Cactus</h1><p>您的验证码为：</p><p style="color: red;">' + Math.random().toString().slice(-6) + '</p>'
     }
-}
 
-transporter.sendMail(options, function(err, msg){
-    if(err){
-        console.log(err)
-    }else{
-        console.log(msg)
-    }
-})
+    transporter.sendMail(options, function(err, msg){
+        if(err){
+            console.log(err)
+            res.send({success: false, message: '邮件发送失败'})
+        }else{
+            console.log(msg)
+            res.send({success: true, message: '邮件发送成功'})
+        }    })
+
+}
