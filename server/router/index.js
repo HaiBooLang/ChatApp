a73216ce6f237551cbd29697caeefd5117dc4d9d
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-25 09:15:54 +0800

    Update index.js

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,6 @@
 var dbserver = require('../dao/dbserver')
 var emailserver = require('../dao/emailserver')
-
+var signup = require('../server/signup')
 /**
  * Sets up the router for the application.
  * @param {Object} app - The Express application object.
@@ -13,5 +13,9 @@ module.exports = function(app){
     app.post('/mail', (req, res) => {
         emailserver.emailSignUp(req.body.mail)
         res.send({success: true, message: '邮件发送成功'})
+    })
+
+    app.post('/signup/add', (req, res) => {
+        signup.signUp(req, res)
     })
 }
