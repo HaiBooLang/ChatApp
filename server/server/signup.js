360fbacb0cd8fee35f3ab57b5abeb3285c59b357
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-25 09:17:51 +0800

    Update signup.js

--- server/server/signup.js
+++ server/server/signup.js
@@ -1,9 +1,26 @@
-var dbserver = require('../dao/dbserver');
+const dbserver = require('../dao/dbserver');
 
+/**
+ * Creates a new user account with the provided email, name and password.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
 exports.signUp = function (req, res) {  
     let mail = req.body.mail
     let name = req.body.name
     let pwd = req.body.pwd
 
     dbserver.buildUser(mail, mail, pwd, res)
+}
+
+/**
+ * Checks if a given user value already exists in the database.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+exports.judgeValue = function (req, res) {
+    let data = req.body.data
+    let type = req.body.type
+
+    dbserver.countUserValue(data, type, res)
 }
