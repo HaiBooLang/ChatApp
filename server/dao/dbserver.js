afa2143b8eb365df62cb4fb8a9e094685c71cc68
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-25 09:11:12 +0800

    Update dbserver.js

--- server/dao/dbserver.js
+++ server/dao/dbserver.js
@@ -3,15 +3,49 @@
  * @param {Object} res - The response object.
  * @returns {void}
  */
+var bcrypt = require('../model/bcrypt')
 var dbmodel = require('../model/dbmodel')
 var User = dbmodel.model('User')
 
-exports.findUser = function(res) {
-    User.find({}, function(err, doc) {
-        if (err) {
-            console.log(err)
+/**
+ * Builds a new user and saves it to the database.
+ * @param {string} name - The name of the user.
+ * @param {string} mail - The email of the user.
+ * @param {string} pwd - The password of the user.
+ * @param {Object} res - The response object.
+ * @returns {void}
+ */
+exports.buildUser = function (name, mail, pwd, res) {
+    let password = bcrypt.encryption(pwd)
+
+    let data ={
+        name: name,
+        email: mail,
+        psw: password,
+        time: new Date(),  
+    }
+
+    let user = new User(data)
+
+    user.save(function(err, result) {
+        if(err) {
+            res.send({status:500, success: false, message: '注册失败'})
         } else {
-            res.send(doc)
+            res.send({status:200, success: true, message: '注册成功'})
+        }
+    })
+}
+
+exports.countUserValue = function (data, type, res) {
+    let wherestr = {}
+
+    wherestr[type] = data
+    
+    User.countDocuments(wherestr, function(err, result) {
+        if(err) {
+            res.send({status:500, success: false, message: '查询失败'})
+        } else {
+            res.send({status:200, success: true, message: '查询成功', result: result})
         }
     })
 }
