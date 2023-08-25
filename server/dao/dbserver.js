efc2290bc74e3d2b014616b598eaf7e6d2594bd3
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-08-25 16:00:28 +0800

    Update dbserver.js

--- server/dao/dbserver.js
+++ server/dao/dbserver.js
@@ -7,6 +7,9 @@ var bcrypt = require('../dao/bcrypt')
 var jwt = require('../dao/jwt')
 var dbmodel = require('../model/dbmodel')
 var User = dbmodel.model('User')
+var Friend = dbmodel.model('Friend')
+var Group = dbmodel.model('Group')
+var GroupUser = dbmodel.model('GroupUser')
 
 /**
  * Builds a new user and saves it to the database.
@@ -109,4 +112,86 @@ exports.userMatch = function (data, pwd, res) {
             }
         }
     })
+}
+
+/**
+ * Searches the database for users with names or emails that match the provided data.
+ * @param {string} data - The data to search for.
+ * @param {Object} res - The response object.
+ * @returns {void}
+ */
+exports.searchUser = function (data, res) {
+  if (data == 'cactus') {
+    let wherestr = {}
+  } else {
+    let wherestr = { $or: [{ 'name': { $regex: data } }, { 'email': { $regex: data } }] }
+  }
+  let out = {
+    'name': 1,
+    'email': 1,
+    'imgurl': 1
+  }
+  User.find(wherestr, out, function (err, result) {
+    if(err) {
+      res.send({ status: 500, success: false, message: '查询失败' })
+    } else {
+      res.send({ status: 200, success: true, message: '查询成功', result: result })
+    }
+  })
+}
+
+/**
+ * Searches the database for a friend relationship between two users.
+ * @param {string} uid - The user ID.
+ * @param {string} fid - The friend ID.
+ * @param {Object} res - The response object.
+ * @returns {void}
+ */
+exports.isFriend = function (uid, fid, res) {
+  let wherestr = { 'userID': uid, 'friendID': fid, 'state': 0}
+  Friend.findOne(wherestr, function (err, result) {
+    if (err) {
+      res.send({ status: 500, success: false, message: '查询失败' })
+    } else {
+      if (result) {
+        res.send({ status: 200, success: true, message: '查询成功', result: true })
+      } else {
+        res.send({ status: 400, success: true, message: '查询成功', result: false })
+      }
+    }
+  })
+}
+
+exports.searchGroup = function (data, res) {
+  if (data == 'cactus') {
+    let wherestr = {}
+  } else {
+    let wherestr ={ 'name': { $regex: data } }
+  }
+  let out = {
+    'name': 1,
+    'imgurl': 1
+  }
+  Group.find(wherestr, out, function (err, result) {
+    if(err) {
+      res.send({ status: 500, success: false, message: '查询失败' })
+    } else {
+      res.send({ status: 200, success: true, message: '查询成功', result: result })
+    }
+  })
+}
+
+exports.isInGroup = function (uid, gid, res) {
+  let wherestr = { 'userID': uid, 'groupID': gid }
+  GroupUser.findOne(wherestr, function (err, result) {
+    if (err) {
+      res.send({ status: 500, success: false, message: '查询失败' })
+    } else {
+      if (result) {
+        res.send({ status: 200, success: true, message: '查询成功', result: true })
+      } else {
+        res.send({ status: 400, success: true, message: '查询成功', result: false })
+      }
+    }
+  })
 }
